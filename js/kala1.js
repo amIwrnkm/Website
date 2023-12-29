104b81819c8707e8962221989ea9398854774403
Amirhosein <149807572+amIwrnkm@users.noreply.github.com>
2023-12-29 19:01:42 +0330

    add new kala and mini modifaided in the all page

--- js/kala1.js
+++
@@ -1,6 +0,0 @@
- var kala = "<div><a href='/Kala/html/Mobile/Test.html'><img src='/img/laptap test.png' class='photokala'><p class='boldkala'> اسم محصول </p><p class='boldkala'>قیمت:10,000,000 </p></a></div>";
- var kala1 = "<div class='del'><a href='/Kala/html/Mobile/Test.html'><img src='/img/laptap test.png' class='photokala'><p class='boldkala'> اسم محصول </p><p class='boldkala'>قیمت:10,000,000 </p></a></div>";
- var kalaAsl = "<div class='titrkala'><p class='bold'>اسم برند</p><p class='alak'>ه</p></div><div class='kala'>"+kala+kala+kala+kala1+kala1+"</div>";
- var sectiun ="<div class='kala-asl'>"+kalaAsl+kalaAsl+kalaAsl+kalaAsl+kalaAsl+"</div>";
- var secfld = document.getElementById("sec");
- secfld.innerHTML = sectiun;
